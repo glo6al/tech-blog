a784140273ca40bb5cb8ea2ad1904b0e96b6e007
glo6al <towers.a@gmail.com>
2022-08-23 18:54:26 -0400

    connect seeds and db

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -4,7 +4,8 @@ const postData = [
   {
     title: "What is an MVC? ",
 
-    text: "An MVC or Model View Controller, is a software design pattern for developing web applications. It divides a given application into three interconnected parts in order to separate internal representations",
+    post_body:
+      "An MVC or Model View Controller, is a software design pattern for developing web applications. It divides a given application into three interconnected parts in order to separate internal representations",
 
     user_id: 1,
   },
@@ -12,7 +13,8 @@ const postData = [
   {
     title: "How to use different CSS selectors",
 
-    text: 'CSS selectors are used to "select" the content you want to style. Selectors are patterns that match against elements in a tree structure',
+    post_body:
+      'CSS selectors are used to "select" the content you want to style. Selectors are patterns that match against elements in a tree structure',
 
     user_id: 2,
   },
@@ -20,7 +22,8 @@ const postData = [
   {
     title: "Getting Started with HTML",
 
-    text: "HTML is the standard markup language for Web pages. HTML describes the structure of a Web page semantically and originally included cues for the appearance of the document.",
+    post_body:
+      "HTML is the standard markup language for Web pages. HTML describes the structure of a Web page semantically and originally included cues for the appearance of the document.",
 
     user_id: 3,
   },
@@ -28,7 +31,8 @@ const postData = [
   {
     title: "Why you should learn JS",
 
-    text: "JavaScript is a programming language that adds interactivity to your website (for example games, responses when buttons are pressed or data entered in forms).",
+    post_body:
+      "JavaScript is a programming language that adds interactivity to your website (for example games, responses when buttons are pressed or data entered in forms).",
 
     user_id: 4,
   },
@@ -36,7 +40,8 @@ const postData = [
   {
     title: "Responsive Web Design with Bootstrap",
 
-    text: "Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS- and JavaScript-based design templates for typography, forms, buttons, navigation",
+    post_body:
+      "Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS- and JavaScript-based design templates for typography, forms, buttons, navigation",
 
     user_id: 5,
   },
@@ -44,7 +49,8 @@ const postData = [
   {
     title: "Getting Started with Git",
 
-    text: "Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.",
+    post_body:
+      "Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.",
 
     user_id: 6,
   },
