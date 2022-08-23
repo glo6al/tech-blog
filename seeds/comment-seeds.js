e020e0d8b009e749b393e9d0bacb7f8138134d98
glo6al <towers.a@gmail.com>
2022-08-23 15:10:20 -0400

    add seeds to comment-seeds.js

---
+++ seeds/comment-seeds.js
@@ -0,0 +1,77 @@
+const { Comment } = require("../models");
+
+const commentData = [
+  {
+    comment_text: "This is great. Thanks for sharing!",
+
+    user_id: 6,
+
+    post_id: 1,
+  },
+
+  {
+    comment_text: "What packages does SQL work with?",
+
+    user_id: 3,
+
+    post_id: 2,
+  },
+
+  {
+    comment_text: "How do I get started with coding?",
+
+    user_id: 2,
+
+    post_id: 3,
+  },
+
+  {
+    comment_text: "This is really helpful. Thanks for writing this!",
+
+    user_id: 4,
+
+    post_id: 4,
+  },
+
+  {
+    comment_text: "Can you recommend any good books on coding?",
+
+    user_id: 1,
+
+    post_id: 5,
+  },
+
+  {
+    comment_text: "This is a great resource. Thanks for sharing!",
+
+    user_id: 5,
+
+    post_id: 6,
+  },
+
+  {
+    comment_text: "I'm just getting started with coding. Any tips?",
+
+    user_id: 2,
+
+    post_id: 7,
+  },
+  {
+    comment_text: "This is really helpful. Thanks for writing this!",
+
+    user_id: 4,
+
+    post_id: 8,
+  },
+  {
+    comment_text: "Can you recommend any good books on coding?",
+
+    user_id: 1,
+
+    post_id: 5,
+  },
+];
+
+const seedComments = () => Comment.bulkCreate(commentData);
+
+module.exports = seedComments;
