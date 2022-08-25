7d361b43969059a57b6be6a456a661d5fc42683e
glo6al <towers.a@gmail.com>
2022-08-25 13:23:33 -0400

    add single post to signle-post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,46 +1,49 @@
-const router = require('express').Router();
-const { Post, Comment, User } = require('../models/');
+const router = require("express").Router();
+const { Post, Comment, User } = require("../models/");
 
 // get all posts for homepage
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     // we need to get all Posts and include the User for each (change lines 8 and 9)
     const postData = await SomeModel.someSequelizeMethod({
-      include: [{
-        model: Comment,
-        attributes: [
-          "id",
-          "comment_text",
-          "post_id",
-          "user_id",
-          "created_at",
-        ],
-        include: {
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "post_id",
+            "user_id",
+            "created_at",
+          ],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+        {
           model: User,
           attributes: ["username"],
         },
-      },
-      {
-        model: User,
-        attributes:["username"],
-      }],
+      ],
     });
     // serialize the data
     const posts = postData.map((post) => post.get({ plain: true }));
     // we should render all the posts here
-    res.render('all-posts', { posts, loggedIn: require.session.loggedIn });
+    res.render("all-posts", { posts, loggedIn: require.session.loggedIn });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 // get single post
-router.get('/post/:id', async (req, res) => {
+router.get("/post/:id", async (req, res) => {
   try {
     // what should we pass here? we need to get some data passed via the request body (something.something.id?)
     // change the model below, but not the findByPk method.
-    const postData = await SomeModel.findByPk(????, {
+    const postData = await SomeModel.findByPk(req.params.id, {
       // helping you out with the include here, no changes necessary
+      attributes: ["id", "post_body", "title", "created_at"],
       include: [
         User,
         {
@@ -54,7 +57,7 @@ router.get('/post/:id', async (req, res) => {
       // serialize the data
       const post = postData.get({ plain: true });
       // which view should we render for a single-post?
-      res.render('hmmmm what view should we render?', { post });
+      res.render("single-post", { post, loggedIn: req.session.loggedIn });
     } else {
       res.status(404).end();
     }
@@ -64,22 +67,22 @@ router.get('/post/:id', async (req, res) => {
 });
 
 // giving you the login and signup route pieces below, no changes needed.
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect('/');
+    res.redirect("/");
     return;
   }
 
-  res.render('login');
+  res.render("login");
 });
 
-router.get('/signup', (req, res) => {
+router.get("/signup", (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect('/');
+    res.redirect("/");
     return;
   }
 
-  res.render('signup');
+  res.render("signup");
 });
 
 module.exports = router;
